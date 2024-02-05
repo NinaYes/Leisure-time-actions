bdb32cef191f19837a202874a1103931deb4b47c
NinaYes <nina.esipova@gmail.com>
2024-02-04 22:10:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [activity, setActivity] = useState("");
+
+  useEffect(() => {
+     getActivity()
+  }, [])
+
+    const getActivity = async () => {
+    const response = await fetch (`http://www.boredapi.com/api/activity/ `);
+    const data = await response.json();
+    console.log(data.activity);
+    setActivity(data.activity);
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{activity}</h1>
+      <button onClick={getActivity}>Click here to get a new advice</button>
     </div>
   );
 }
